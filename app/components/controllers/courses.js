e513e32e13b7aebf5a571551490dbf984ad255bc
Tri Yuniarti <Tri Yuniarti>
2016-07-21 17:00:40 +0700

    fix bugs

--- app/components/controllers/courses.js
+++ app/components/controllers/courses.js
@@ -1,13 +1,13 @@
 'use strict';
 
-angular.module('app.courses', ['ngRoute', 'angular-ckeditor'])
+angular.module('app.courses', ['ngRoute'])
 
 .controller('CoursesController', ['$http', '$scope', function($http, $scope) {
       $http({
         method: 'GET',
         url: 'http://128.199.255.96/v1/training/courses'
       }).success(function (response) {
-          $scope.courses = response.data;
+          $scope.course = response.data;
       }).error(function (response) {
           alert(response.message);
       })
